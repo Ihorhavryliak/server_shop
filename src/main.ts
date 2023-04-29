ed222ac13e4578558872e4ba0016e907a9efef8c
Ihor <kachkamala@gmail.com>
2023-04-29 19:23:03 +0300

    chege port

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(process.env.PORT, '');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
